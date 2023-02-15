3cfe381450b98327fb2468765ecf653ebee310ce
Waterhorse928 <72754883+Waterhorse928@users.noreply.github.com>
2023-02-14 16:44:54 -0800

    ai gibberish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-
+import random
+# All characters have these stats.
 class Character:
     def __init__(self, name="", hp=0):
         self.name = name
@@ -15,6 +16,11 @@ class Reimu(Character):
     def update(self):
         self.info = f'Graze {self.sp}'
 
+# Attacks and Skills
+    def homingAmulet(self, target):
+        target.hp -= 4
+        print(f"{self.name} used Homing Amulet! {target.name} took 4 damage.")
+
 class Marisa(Character):
     def __init__(self):
         super().__init__("Marisa Kirisame", 24)
@@ -23,7 +29,6 @@ class Marisa(Character):
 
     def update(self):
         self.info = f'Magic {self.sp}'
-
 
 class Boss:
     def __init__(self, name="", hp=0):
@@ -39,6 +44,17 @@ class Marisa_Boss(Boss):
 
     def update(self):
         self.info = f'Magic {self.sp}'
+
+    def MagicMissile(self, target):
+        target.hp -= 2
+        print(f"{self.name} casts Magic Missile! {target.name} takes 2 damage.")
+
+    def MasterSpark(self, target):
+        damage = 15
+        target.hp -= damage
+        self.sp = 0
+        self.update()
+        print(f"{self.name} casts Master Spark! {target.name} takes {damage} damage. {self.name}'s Magic is now 0.")
 
 def box(text):
     boxSize = 100
@@ -69,7 +85,10 @@ def healthbar(HP,maxHP,size):
 def displayBattleScreen(boss,chars):
     characters = [char for char in (chars) if char != 0]
     numberOfChar = len(characters)
-    boxList1 = [[boss.name],[f"Health {boss.hp}/{boss.hpMax}"],[healthbar(boss.hp,boss.hpMax,50)],[boss.info]]
+    boxList1 = [[boss.name],
+                [f"Health {boss.hp}/{boss.hpMax}"],
+                [healthbar(boss.hp,boss.hpMax,50)],
+                [boss.info]]
     boxList2 = [[char.name for char in characters],
                 [f"HP {char.hp}/{char.hpMax}" for char in characters],
                 [healthbar(char.hp,char.hpMax,10) for char in characters],
@@ -78,11 +97,26 @@ def displayBattleScreen(boss,chars):
     if numberOfChar > 0:
         box(boxList2)
 
+def battleLoop(boss, chars):
+    characters = [boss] + [char for char in chars if char != 0]
+    turnCounter = 0
+    while boss.hp > 0 and any(char.hp > 0 for char in chars):
+        displayBattleScreen(boss, chars)
+        currentCharacter = characters[turnCounter % len(characters)]
+        if currentCharacter in chars:
+            print(f'{currentCharacter.name} ({healthbar(currentCharacter.hp,currentCharacter.hpMax,10)} HP):', end=' ')
+            print('What would you like to do?')
+            for i, skill in enumerate(currentCharacter.skills):
+                print(f'{i + 1}. {skill.__name__}')
+            choice = int(input('Enter the number of the skill you would like to use: ')) - 1
+            chosenSkill = currentCharacter.skills[choice]
+            target = boss if input('Would you like to attack the boss? (y/n) ') == 'y' else chars[(chars.index(currentCharacter) + 1) % len(chars)]
+            chosenSkill(target)
+        else:
+            bossSkill = random.choice(boss.skills)
+            target = random.choice(chars)
+            bossSkill(target)
+        turnCounter += 1
 
-def battleLoop(boss,chars):
-    while True:
-        displayBattleScreen(boss,chars)
 
-        break
-
-battleLoop(Marisa_Boss(),[Reimu(),Reimu(),Reimu(),Reimu(),Reimu()])
+battleLoop(Marisa_Boss(),[Reimu()])
